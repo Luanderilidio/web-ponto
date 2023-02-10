799ef46f736226870fd2e5eb97a7f38e491f5f7c
Luander <luanderilidio174@gmail.com>
2023-02-10 17:54:06 -0400

    [feat]: altera responsividade para full

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -19,12 +19,12 @@ export default function Register() {
 	return (
 		<Base title='Registrar Frequência'>
 			<Grid container spacing={3}>
-				<Grid item sm={12} md={5}>
-					<Stack spacing={3}>
+				<Grid item xs={12} sm={12} md={12} lg={12}>
+					<Stack spacing={4}>
 						<FormRegister />
 					</Stack>
 				</Grid>
-				<Grid item sm={12} md={7}>
+				<Grid item xs={12} sm={12} md={12} lg={6}>
 					<RegisterPdf />
 				</Grid>
 			</Grid>
