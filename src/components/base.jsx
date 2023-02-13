af1bd90dcae160df8f127ea52680e28bba653c6f
Luander <luanderilidio174@gmail.com>
2023-02-13 12:27:52 -0400

    [style]: troca cor de fundo de base

--- src/components/base.jsx
+++ src/components/base.jsx
@@ -3,7 +3,7 @@ import { SideBar } from './Sidebar'
 
 export function Base(props) {
 	return (
-		<div className='fixed flex w-full h-full dark:bg-blue-gray-900 bg-blue-gray-50 gap-2'>
+		<div className='fixed flex w-full h-full dark:bg-blue-gray-900 bg-blue-gray-100 gap-2'>
 			<SideBar />
 			<div className='overflow-scroll scrol w-full flex flex-col px-4 py-2 gap-16 scrollbar-hide'>
 				<Header title={props.title} />
