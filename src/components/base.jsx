dc85c5f5aa3f5f7f0806a8f1be433c907e1c92de
Luander <luanderilidio174@gmail.com>
2023-02-13 15:43:24 -0400

    [feat]: adiciona footer

--- src/components/base.jsx
+++ src/components/base.jsx
@@ -1,14 +1,19 @@
+import { Footer } from './Footer'
 import { Header } from './Header'
 import { SideBar } from './Sidebar'
 
 export function Base(props) {
 	return (
-		<div className='fixed flex w-full h-full dark:bg-blue-gray-900 bg-blue-gray-100 gap-2'>
+		<div className='fixed flex w-full h-full dark:bg-blue-gray-900 bg-blue-gray-100'>
 			<SideBar />
-			<div className='overflow-scroll scrol w-full flex flex-col px-4 py-2 gap-16 scrollbar-hide'>
-				<Header title={props.title} />
+			<div className='overflow-scroll scrollbar-hide'>
+				<div className=' w-full flex flex-col px-4 py-2 gap-16 '>
+					<Header title={props.title} />
 
-				{props.children}
+					{props.children}
+					<div className='h-3' />
+				</div>
+				<Footer />
 			</div>
 		</div>
 	)
